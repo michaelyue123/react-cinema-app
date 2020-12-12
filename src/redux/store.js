13ba9320001a85df70c601c2c80714dd3b8ada6f
s3796237 <s3796237@student.rmit.edu.au>
2020-12-12 14:35:53 +1100

    created a initial setup for react redux

---
+++ src/redux/store.js
@@ -0,0 +1,18 @@
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+
+import rootReducers from './reducers';
+
+const initialState = {};
+const loggerMiddleware = createLogger();
+const middleware = [thunkMiddleware, loggerMiddleware];
+
+const store = createStore(
+    rootReducers,
+    initialState,
+    composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
